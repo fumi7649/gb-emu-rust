9b56258b61651d389f01d3df3e5ff719343a10b1
kaf <fumifumi7629@gmail.com>
2024-08-20 00:36:31 +0900

    feature/cpu

--- cpu.rs
+++ cpu.rs
@@ -13,4 +13,18 @@ impl Cpu {
         self.regs.pc = self.regs.pc.wrapping_add(1); // プログラムカウンタを進める
         self.ctx.cb = false;
     }
+    pub fn decode(&mut self, bus: &mut &Peripherals) {
+        match self.ctx.opcode {
+            0x00 => self.nop(bus),
+            _    => panic!("Not implemented: {:02x}", self.opcode)
+        }
+    }
+    // 何もしないという命令
+    // fetch/execute overlapにしたがい次の命令をfetchする
+    pub fn nop(&mut self, bus: &Peripherals) {
+        self.fetch(bus);
+    }
+    pub fn emulate_cycle(&mut self, bus: &mut Peripherals) {
+        self.decode(bus);
+    }
 }
