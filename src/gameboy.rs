118bbc75aa25577d795e9cb82ad586ebc7c3ef8f
kaf <fumifumi7629@gmail.com>
2024-09-16 18:32:10 +0900

    ファイルの移動

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -1,7 +1,7 @@
-use std::time;
-pub const CPU_CLOCK_HZ: u128 = 4_194_304;
-pub const M_CYCLE_CLOCK: u128 = 4;
-const M_CYCLE_NANOS: u128 = M_CYCLE_CLOCK * 1_000_000_000 / CPU_CLOCK_HZ;
+// use std::time;
+// pub const CPU_CLOCK_HZ: u128 = 4_194_304;
+// pub const M_CYCLE_CLOCK: u128 = 4;
+// const M_CYCLE_NANOS: u128 = M_CYCLE_CLOCK * 1_000_000_000 / CPU_CLOCK_HZ;
 // ...
 // let time = time::Instant::now();
 // let mut elapsed = 0;
