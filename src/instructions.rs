80939416217056b6eac64b771d7421afedaa5b30
kaf <fumifumi7629@gmail.com>
2024-09-19 22:45:12 +0900

    cp命令の実装

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,12 +4,7 @@ use crate::{
     peripherals::Peripherals,
 };
 
-use std::sync::atomic::{
-    AtomicU8,
-    AtomicU16,
-    Ordering::Relaxed,
-};
-
+use std::sync::atomic::{AtomicU16, AtomicU8, Ordering::Relaxed};
 
 impl Cpu {
     // 何もしないという命令
@@ -56,5 +51,25 @@ impl Cpu {
                 self.fetch(bus);
             },
         });
+    }
+    // Aレジスタとsの値を比較する
+    // Zフラグ：演算結果が0の場合1にする
+    // N 無条件で1にする
+    // H 4bit目からの繰り下がりが発生した場合は1にする
+    // C 8bit目からの繰り下がりが発生した場合は1にする
+    pub fn cp<S: Copy>(&mut self, bus: &Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            let (result, carry) = self.regs.a.overflowing_sub(v);
+            self.regs.set_zf(result == 0);
+            self.regs.set_nf(true);
+            // 0xfは00001111
+            // & 0xfで下位4ビットを抽出できる
+            self.regs.set_hf((self.regs.a & 0xf) < (v & 0xf));
+            self.regs.set_cf(carry);
+            self.fetch(bus);
+        }
     }
 }
