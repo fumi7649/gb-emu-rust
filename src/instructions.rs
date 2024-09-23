a22f8b82f6da769e4c2154dafbb8d787f8335219
kaf <fumifumi7629@gmail.com>
2024-09-23 23:36:23 +0900

    push命令の実装

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,6 +1,6 @@
 use crate::{
     cpu::{go, step, Cpu},
-    operand::{IO16, IO8},
+    operand::{Reg16, IO16, IO8},
     peripherals::Peripherals,
 };
 
@@ -179,5 +179,41 @@ impl Cpu {
             self.fetch(bus);
         }
     }
-
+    pub fn push16(&mut self, bus: &mut Peripherals, val: u16) -> Option<()> {
+        step!(None, {
+            0: {
+                go!(1);
+                return None;
+            },
+            1: {
+                let [lo, hi] = u16::to_le_bytes(val);
+                self.regs.sp = self.regs.sp.wrapping_sub(1);
+                bus.write(self.regs.sp, hi);
+                VAL8.store(lo, Relaxed);
+                go!(2);
+                return None;
+            },
+            2: {
+                self.regs.sp = self.regs.sp.wrapping_sub(1);
+                bus.write(self.regs.sp, VAL8.load(Relaxed));
+                go!(3);
+                return None;
+            },
+            3: {
+                return Some(go!(0));
+            },
+        });
+    }
+    pub fn push(&mut self, bus: &mut Peripherals, src: Reg16) {
+        step!((), {
+            0: {
+                VAL16.store(self.read16(bus, src).unwrap(), Relaxed);
+                go!(1);
+            },
+            1: if self.push16(bus, VAL16.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
 }
